d8cd49719a4d70127eecdd54d2ca08e5a8a15361
qodhrkawk <gabriel1120@naver.com>
2020-11-19 04:10:03 +0900

    기본적 뷰 구현 완료

---
+++ 29cm_iOS/29cm_iOS/Sources/Cells/ProductDetail/ProductPriceCVC.swift
@@ -0,0 +1,17 @@
+//
+//  ProductPriceCVC.swift
+//  29cm_iOS
+//
+//  Created by Yunjae Kim on 2020/11/19.
+//
+
+import UIKit
+
+class ProductPriceCVC: UICollectionViewCell {
+    static let identifier = "ProductPriceCVC"
+    
+    
+    
+    
+    
+}
--- 29cm_iOS/29cm_iOS/Sources/VCs/ProductDetail/ProductDetailVC.swift
+++ 29cm_iOS/29cm_iOS/Sources/VCs/ProductDetail/ProductDetailVC.swift
@@ -8,22 +8,140 @@
 import UIKit
 
 class ProductDetailVC: UIViewController {
-
+    
+    
+    //MARK: - IBOutlets
+    
+    @IBOutlet weak var backButton: UIButton!
+    @IBOutlet weak var wholeCollectionView: UICollectionView!
+    
+    
+    
+    
+    //MARK: - Lifecycle Methods
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        wholeCollectionView.delegate = self
+        wholeCollectionView.dataSource = self
+        
         // Do any additional setup after loading the view.
     }
     
+    
+    
+    
+    //MARK: - User Define Functions
+    
+    
+    
+    
+}
 
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+extension ProductDetailVC : UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 2
     }
-    */
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        if indexPath.item == 0{
+            guard let cell = collectionView.dequeueReusableCell(
+                    withReuseIdentifier: ProductPriceCVC.identifier,
+                    for: indexPath) as? ProductPriceCVC else {
+                
+                return UICollectionViewCell()}
+            
+            
+            return cell
+            
+        }
+        else{
+            guard let cell = collectionView.dequeueReusableCell(
+                    withReuseIdentifier: RelatedCVC.identifier,
+                    for: indexPath) as? RelatedCVC else {
+                
+                return UICollectionViewCell()}
+            
+            
+            return cell
+            
+            
+        }
+        
+        
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        switch kind {
+        case UICollectionView.elementKindSectionHeader:
+            let headerView = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "reusableView", for: indexPath)
+            return headerView
+        default:
+            assert(false,"")
+            
+        }
+        
+        
+    }
+    
+    
+}
 
+extension ProductDetailVC : UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+        
+    }
+    
+    
+    
+    
+    
+    
+}
+
+extension ProductDetailVC : UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        if indexPath.item == 0{
+            return CGSize(width: collectionView.frame.width , height: 162)
+        }
+        else{
+            return CGSize(width: collectionView.frame.width , height: 195)
+        }
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+    }
+    
+    //    UIEdgeInset
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+        
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        let width: CGFloat = collectionView.frame.width
+        let height: CGFloat = 420
+        return CGSize(width: width, height: height)
+    }
+ 
+    
 }
